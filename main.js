dd18b973403ffb0233e2d1479717ed9b6ed3e0fa
RGRishit123 <90038525+RGRishit123@users.noreply.github.com>
2023-03-29 16:36:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,525);
+    canvas.position(850,105);
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw()
+{
+    background('#FF0000');
+    document.getElementById("square_side").innerHTML = "Side of the sqaure will be = " + difference + "px";
+    stroke("#1e14db");
+    fill('#0a0a0a'); 
+    square(noseX, noseY, difference);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("NoseX = " + noseX + "NoseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Difference = " + difference + "left wrist = " + leftWristX + "right wrist = " + rightWristX);
+    }
+}
